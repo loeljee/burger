f1c519487e73a9c90fd2d2d716ec3830d832c6e7
loeljee <loeljee@gmail.com>
2018-11-23 13:00:37 -0800

    good enough

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var app = express();
+
+var burger = require("../models/burger.js");
+
+app.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+app.post("/", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+app.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+app.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+module.exports = app;
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Chicken Cordon Bleu Sandwich",
+0
+),
+(
+"Patty Melt Burger",
+0
+),
+(
+"Double Western Bacon Burger",
+0
+);
